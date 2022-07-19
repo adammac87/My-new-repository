fa4269e2af67a3deb43304ae3e50462808f52e6c
Adam <adam.ja.mac@gmail.com>
2022-07-19 19:10:34 +0100

    final

---
+++ dictionary.py
@@ -0,0 +1,1 @@
+person = { 'name' : 'Bob', 'age' : 30, 'height' : 1.78 }
---
+++ loop.py
@@ -0,0 +1,23 @@
+
+
+students = ["Ada", "Charles", "Bill", "Barbara", "Grace", "Clive"]
+
+for student in students:
+  print(student)
+
+numbers = [10, 3, 89, 24, 13, 500]
+
+list_total = 0
+for number in numbers: 
+  list_total = list_total + number
+  print(list_total)
+
+scores = [10, 8, 10, 9, 8]
+
+# add your code below
+list_total = 0
+for number in scores: 
+  list_total = list_total + number
+  print(list_total)
+
+print(list_total / 5)
