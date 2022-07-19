7bbad4e8422a3f4ecf01e7847a96b5c9f906bf15
Adam <adam.ja.mac@gmail.com>
2022-07-19 15:27:48 +0100

    extra

---
+++ function.py
@@ -0,0 +1,18 @@
+def currency_converter():
+  print ("My Currency Converter")
+
+currency_converter() 
+
+def greet(name):
+    print("Hello " + name)
+
+greet("adam")
+
+greet("Wilma")
+greet("Fred")
+greet("Betty")
+greet("Barney")
+
+def print_full_name(first_name,last_name):
+    print(first_name + " " + last_name)
+print_full_name("Adam","Macpherson")
