b3d63e442fa1c50fdcf4fc3ce4b534910b4e5a80
Adam <adam.ja.mac@gmail.com>
2022-07-19 13:25:51 +0100

    added more files

--- tester2.py
+++ tester2.py
@@ -3,3 +3,14 @@
 name  = input("What is your name? ")
 
 print ( "hello" + " "+ name + "!")
+
+user_input = input("What is your age? ")
+# => What is your age? 
+
+  # user input is a string
+# => '21'
+
+age = int(user_input) # converting user input to an int and storing the int value in age
+
+print (age)
+# => 21
