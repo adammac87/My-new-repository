3fb84adfa6abc7f3e1bb54aa3a84db03d036142a
Adam <adam.ja.mac@gmail.com>
2022-07-19 16:28:47 +0100

    update update

--- function.py
+++ function.py
@@ -16,3 +16,36 @@ greet("Barney")
 def print_full_name(first_name,last_name):
     print(first_name + " " + last_name)
 print_full_name("Adam","Macpherson")
+
+def currency_converter(rate,amount):
+  print("My Currency Converter")
+currency_converter("rate","amount") 
+
+def currency_converter(rate,amount):
+    print("My Currecncy Converter")
+currency_converter(1,2) 
+
+def currency_converter(rate,amount):
+  print(rate+ amount)
+currency_converter("1", "2") 
+
+def greet(name):
+    print("Hello " + name)
+
+name_1 = "Jack"
+name_2 = "Victor"
+name_3 = "Isa"
+
+greet(name_1)
+greet(name_2)
+greet(name_3)
+
+def currency_converter(rate, amount):
+    print("Converting " + str(amount) + " of the original currency to the new currency at a rate of " + str(rate) + " of the new currency to 1 of the original currency")
+
+# add your code below
+pounds=8
+drogna=2 
+currency_converter(drogna, pounds)
+
+
